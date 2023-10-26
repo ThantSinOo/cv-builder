27b6c2f9ea89bb740e0c689f4a06e3ad579dc8d0
Thant Sin Oo <mgthantsinmm21@gmail.com>
2023-10-26 14:18:37 +0630

    project setup and upload

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../style/WelcomeStyle.css'
+function Welcome() {
+  return (
+    <div>
+        <h1 className='header'>Welcome From CV-Builder</h1>
+    </div>
+  )
+}
+
+export default Welcome
