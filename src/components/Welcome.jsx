eb0860c7e4e672eeab935c59d60bcdd7d5631add
Thant Sin Oo <mgthantsinmm21@gmail.com>
2023-10-26 15:08:39 +0630

    add welcom page

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import '../style/WelcomeStyle.css'
 function Welcome() {
+
+    let getStarted = () =>{
+        console.log("Get Started Work!!")
+    }
+
   return (
-    <div>
-        <h1 className='header'>Welcome From CV-Builder</h1>
+    <div className='welcomeContainer'>
+      <div className='welcomeHeader'>
+        <h1 className='header'>Welcome</h1>
+        <h2 className='subHeader'>from CV-Builder</h2>
+        <p className='welcomeText'>Welcome to our CV creation platform! Design your CV templates and get instant shareable links for job applications.<span className='motto'>Simplify recruitment with us.</span></p>
+      </div>
+      <div>
+        <button 
+            className='getStartBtn'
+            onClick={getStarted}
+            >
+                Get Started!
+        </button>
+      </div>
     </div>
   )
 }
